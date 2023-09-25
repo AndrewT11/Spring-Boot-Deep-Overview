1c619e2218e022788a871e145ac2aaa6976868e1
Andrew Tran <tranandrew@gmail.com>
2023-09-25 11:22:33 -0500

    adding property values to application.properties. Using @Value annotation at endpoint to return property values in browser

--- 01-sping-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-sping-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,11 +2,23 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class FunRestController {
-    // expose "/" that will return "Hello World"
+    // Inject properties for team name and coach name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
 
+    // expose endpoints for teaminfo values
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach name: " + coachName + ", Team name: " + teamName;
+    }
+
+    // expose "/" that will return "Hello World"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
