4876c6efe468078df2444811995bf64ffa3f0ecd
Andrew Tran <tranandrew@gmail.com>
2023-10-04 09:58:21 -0500

    bean lifecycle @PostConstruct and @PreDestroy annotations used to call custom business logic methods, settung up handles to resources (db, sockets, files) during bean initialization and the same for bean destruction

---
+++ 02-spring-boot-spring-core/08 - bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,30 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define private field for dependency
+    private Coach myCoach;
+
+
+   @Autowired
+   public DemoController(@Qualifier("cricketCoach") Coach theCoach,
+                         @Qualifier("cricketCoach") Coach theAnotherCoach) {
+
+       System.out.println("In Constructor: " + getClass().getSimpleName());
+
+       myCoach = theCoach;
+
+   }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
