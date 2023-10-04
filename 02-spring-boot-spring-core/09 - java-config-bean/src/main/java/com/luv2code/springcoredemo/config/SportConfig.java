d0f4f47d83eedded1bed6960d384214af0ceba79
Andrew Tran <tranandrew@gmail.com>
2023-10-04 13:55:04 -0500

    create config package/SportConfig.java for creating @Bean annotated injection add SQL files to create user and student_tracker table

--- 02-spring-boot-spring-core/09 - java-config-bean/src/main/java/com/luv2code/springcoredemo/config/SportConfig.java
+++ 02-spring-boot-spring-core/09 - java-config-bean/src/main/java/com/luv2code/springcoredemo/config/SportConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class SportConfig {
 
     // manually handling the construction of swim coach
-    // bean id defaults to the method name
+    // bean id defaults to the method name, we add a custom name here
     @Bean("aquatic")
     public Coach swimCoach() {
         return new SwimCoach();
