ed37a5009f77f1aa0c97138cfb9d9ceeb6afafb2
Andrew Tran <tranandrew@gmail.com>
2023-10-04 08:47:16 -0500

    @lazy annotation to TrackCoach

--- 02-spring-boot-spring-core/06 - lazy-initialization/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
+++ 02-spring-boot-spring-core/06 - lazy-initialization/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -1,11 +1,14 @@
 package com.luv2code.springcoredemo.common;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+    public TennisCoach() {
+        System.out.println("In Constructor: " + getClass().getSimpleName());
+    }
 
-    @Override
     public String getDailyWorkout() {
         return "30 mins volley practice";
     }
