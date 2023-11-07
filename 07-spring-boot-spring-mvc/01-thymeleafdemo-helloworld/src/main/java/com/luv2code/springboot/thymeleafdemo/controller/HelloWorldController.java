fd9c01f9ccd8e62ef4c67a7a64bb8a5e32b43fcc
Andrew Tran <tranandrew@gmail.com>
2023-11-07 17:12:11 -0600

    new handler method processFormVersionThree() using @RequestParam to bind request parameter

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -38,6 +39,21 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert data to all Uppercase
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "V3 name using @RequestParam annotation: " + theName;
 
         // add message to the model
         model.addAttribute("message", result);
