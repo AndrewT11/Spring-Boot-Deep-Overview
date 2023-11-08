31a78ca5398cc8da3b2d04bf0c36a28ad3014877
Andrew Tran <tranandrew@gmail.com>
2023-11-08 16:02:07 -0600

    add countries and options to application.properties add country field with getter/setters to Student class add option field using each loop to dynamically add countries to list instead of hardcode add span to display country of student in student-confirmation.html add List<string> countries to StudentController using @Value annotation to read from application.properties file addAttribute countries to model

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -19,6 +25,9 @@ public class StudentController {
         // add student object to the model (model attribute)
         theModel.addAttribute("student", theStudent);
 
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
+
         return "student-form";
     }
 
@@ -26,7 +35,7 @@ public class StudentController {
     public String processForm(@ModelAttribute("student") Student theStudent) {
 
         // log the input data
-        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName() + " " + theStudent.getCountry());
 
         return "student-confirmation";
     }
