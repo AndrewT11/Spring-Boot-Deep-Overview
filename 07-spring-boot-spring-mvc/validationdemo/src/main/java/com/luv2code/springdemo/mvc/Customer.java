8faa75b4ab2ea9e1b0fb3948be275ba1cf04b927
Andrew Tran <tranandrew@gmail.com>
2023-11-13 18:04:33 -0600

    add freePasses field to Customer class with @Min and @Max annotations Add Free passes input to customer-form.html with error span

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -10,6 +12,18 @@ public class Customer {
     @NotNull(message="is required")
     @Size(min=1, message="is required")
     private String lastName;
+
+    @Min(value=0, message="must be greater than or equal to 0")
+    @Max(value=10, message="must be less than or equal to 10")
+    private int freePass;
+
+    public int getFreePass() {
+        return freePass;
+    }
+
+    public void setFreePass(int freePass) {
+        this.freePass = freePass;
+    }
 
     public String getFirstName() {
         return firstName;
