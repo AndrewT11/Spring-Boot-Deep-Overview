8a3251a9aece03c0ba1758b9e8cfa8ebc8de8c3c
Andrew Tran <tranandrew@gmail.com>
2023-11-15 21:34:00 -0600

    added null logic to isValid

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -15,7 +15,13 @@ public class CourseCodeConstraintValidator implements ConstraintValidator<Course
     @Override
     public boolean isValid(String theCode, ConstraintValidatorContext TheConstraintValidatorContext) {
 
-        boolean result = theCode.startsWith(coursePrefix);
+        boolean result;
+
+           if (theCode != null) {
+               result  = theCode.startsWith(coursePrefix);
+           } else {
+               return true;
+           }
 
         return result;
     }
