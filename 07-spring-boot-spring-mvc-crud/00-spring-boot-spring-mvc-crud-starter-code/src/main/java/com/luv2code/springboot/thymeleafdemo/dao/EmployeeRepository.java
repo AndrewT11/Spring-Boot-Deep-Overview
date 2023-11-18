86a7e845c14dc9aa5e9456cbe6ec2583506c225c
Andrew Tran <tranandrew@gmail.com>
2023-11-17 18:52:58 -0600

    create new custom JPA Repo method for findng all employees by last name asc modify findAll method in EmployeeServiceImpl findAll() method to use new sorted last name method

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,8 +3,15 @@ package com.luv2code.springboot.thymeleafdemo.dao;
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    // that's it ... no need to write any code LOL!
+    // JPARepository will create common CRUD methods for you
+
+    // add a custom method to sort by last name to JPA Repository
+    // Spring Data JPA will parse the method name. Looks for a specfic format and pattern
+    // Create appropriate query ... behind the scenes
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
