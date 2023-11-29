faeefc8e71f4c0683f006642a416b03aef7ed459
Andrew Tran <tranandrew@gmail.com>
2023-11-28 20:50:15 -0600

    leaders.html - create page DemoController.java - Map to /leaders endpoint, return leaders page

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/demo/springboot/demosecurity/controller/DemoController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/demo/springboot/demosecurity/controller/DemoController.java
@@ -10,5 +10,11 @@ public class DemoController {
         return "home";
     }
 
+    // add a request mapping for /leaders
+    @GetMapping("/leaders")
+    public String showLeaders() {
+
+        return "leaders";
+    }
 
 }
